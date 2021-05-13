e1a3498c71db09754389e3af0b3a4b7faf53b160
Oleg Kyselov <olegkycelov@gmail.com>
2021-05-13 22:55:54 +0400

    Refactoring stage #2

---
+++ src/api/fetchData.ts
@@ -0,0 +1,32 @@
+import xml2js from 'xml2js';
+import * as moment from 'moment';
+
+import { capitalizeFirstLetter } from '#root/src/utils';
+
+export async function fetchData() {
+  const [patientMainInfo, weightObservations, heightObservations] = await Promise.all(
+    await Promise.all(
+      await Promise.all([
+        fetch('https://open-ic.epic.com/FHIR/api/FHIR/DSTU2/Patient/Tbt3KuCY0B5PSrJvCu2j-PlK.aiHsu2xUjUM8bWpetXoB'),
+        fetch(
+          'https://open-ic.epic.com/FHIR/api/FHIR/DSTU2/Observation?patient=Tbt3KuCY0B5PSrJvCu2j-PlK.aiHsu2xUjUM8bWpetXoB&code=29463-7'
+        ),
+        fetch(
+          'https://open-ic.epic.com/FHIR/api/FHIR/DSTU2/Observation?patient=Tbt3KuCY0B5PSrJvCu2j-PlK.aiHsu2xUjUM8bWpetXoB&code=8302-2'
+        ),
+      ]).then((requests) => requests.map((request) => request.text()))
+    ).then((requestsText) => requestsText.map((requestText) => xml2js.parseStringPromise(requestText)))
+  );
+
+  const dob = patientMainInfo.Patient.birthDate[0].$.value;
+  const sex = patientMainInfo.Patient.gender[0].$.value;
+  const weightKg = weightObservations.Bundle.entry[0].resource[0].Observation[0].valueQuantity[0].value[0].$.value;
+  const heightSm = heightObservations.Bundle.entry[0].resource[0].Observation[0].valueQuantity[0].value[0].$.value;
+
+  return {
+    age: String(moment().diff(moment(dob, 'YYYY-MM-DD'), 'years')),
+    sex: capitalizeFirstLetter(sex),
+    weight: weightKg,
+    height: heightSm,
+  };
+}
--- src/components/Application/Application.tsx
+++ src/components/Application/Application.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import xml2js from 'xml2js';
-import * as moment from 'moment';
 
 import { InfoTabs } from './components/InfoTabs';
 import { CreatinineClearanceForm } from './components/CreatinineClearanceForm';
 import { creatinineClearance } from '#root/src/texts/advice';
-import { capitalizeFirstLetter, calculateCreatinineClearance } from '#root/src/utils';
+
+import { fetchData } from '#root/src/api';
+
+import { calculateCreatinineClearance } from '#root/src/utils';
 import { CreatinineClearanceCalculationData } from '#root/src/interfaces/CreatinineClearanceCalculationData';
 
 import './styles.css';
@@ -16,35 +17,10 @@ export const Application = () => {
   const [calcaulationResults, setCalcaulationResults] = React.useState(null);
 
   React.useEffect(() => {
-    async function fetchData() {
-      const [patientMainInfo, weightObservations, heightObservations] = await Promise.all(
-        await Promise.all(
-          await Promise.all([
-            fetch('https://open-ic.epic.com/FHIR/api/FHIR/DSTU2/Patient/Tbt3KuCY0B5PSrJvCu2j-PlK.aiHsu2xUjUM8bWpetXoB'),
-            fetch(
-              'https://open-ic.epic.com/FHIR/api/FHIR/DSTU2/Observation?patient=Tbt3KuCY0B5PSrJvCu2j-PlK.aiHsu2xUjUM8bWpetXoB&code=29463-7'
-            ),
-            fetch(
-              'https://open-ic.epic.com/FHIR/api/FHIR/DSTU2/Observation?patient=Tbt3KuCY0B5PSrJvCu2j-PlK.aiHsu2xUjUM8bWpetXoB&code=8302-2'
-            ),
-          ]).then((requests) => requests.map((request) => request.text()))
-        ).then((requestsText) => requestsText.map((requestText) => xml2js.parseStringPromise(requestText)))
-      );
-
-      const dob = patientMainInfo.Patient.birthDate[0].$.value;
-      const sex = patientMainInfo.Patient.gender[0].$.value;
-      const weightKg = weightObservations.Bundle.entry[0].resource[0].Observation[0].valueQuantity[0].value[0].$.value;
-      const heightSm = heightObservations.Bundle.entry[0].resource[0].Observation[0].valueQuantity[0].value[0].$.value;
-
-      setPrefilledValues({
-        age: String(moment().diff(moment(dob, 'YYYY-MM-DD'), 'years')),
-        sex: capitalizeFirstLetter(sex),
-        weight: weightKg,
-        height: heightSm,
-      });
-    }
-
-    fetchData();
+    (async () => {
+      const data = await fetchData();
+      setPrefilledValues(data);
+    })();
   }, []);
 
   const handleCreatinineClearanceFormSubmit = React.useCallback((formData) => {
